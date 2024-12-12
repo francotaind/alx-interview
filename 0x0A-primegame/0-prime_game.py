c024988f179b28552566a52f27cb0325a686f0af
francotaind <francotaind00@gmail.com>
2024-12-12 21:59:38 +0300

    syntax fixes

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,40 +1,38 @@
 #!/usr/bin/python3
+"""Prime game winner determination"""
+
 
 def isWinner(x, nums):
-    """Determine the winner of the prime game."""
-    if x <= 0 or not nums:
+    """Prime game winner determination"""
+    if not nums or x < 1:
         return None
 
-    # Helper function to generate a list of primes up to n using Sieve of Eratosthenes
-    def sieve(n):
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False  # 0 and 1 are not prime
-        for i in range(2, int(n**0.5) + 1):
-            if primes[i]:
-                for j in range(i * i, n + 1, i):
-                    primes[j] = False
-        return primes
+    max_num = max(nums)
 
-    # Precompute primes for the largest n in nums
-    max_n = max(nums)
-    primes = sieve(max_n)
+    # Sieve of Eratosthenes to find all primes up to max_num
+    is_prime = [True] * (max_num + 1)
+    is_prime[0] = is_prime[1] = False
 
-    # Count cumulative primes up to each number
-    prime_count = [0] * (max_n + 1)
-    for i in range(1, max_n + 1):
-        prime_count[i] = prime_count[i - 1] + (1 if primes[i] else 0)
+    for start in range(2, int(max_num**0.5) + 1):
+        if is_prime[start]:
+            for multiple in range(start*start, max_num + 1, start):
+                is_prime[multiple] = False
 
+    # Count the number of primes up to each number
+    prime_count = [0] * (max_num + 1)
+    for i in range(1, max_num + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if is_prime[i] else 0)
+
+    # Determine the winner for each game
     maria_wins = 0
     ben_wins = 0
 
-    # Determine the winner for each round
     for n in nums:
-        if prime_count[n] % 2 == 1:  # Maria wins if the number of primes is odd
+        if prime_count[n] % 2 == 1:
             maria_wins += 1
-        else:  # Ben wins if the number of primes is even
+        else:
             ben_wins += 1
 
-    # Determine overall winner
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
